658369d7728b29e954561ffc2f6ea4f3b5243120
shiqiao <lin@qq.com>
2022-01-10 17:50:12 +0800

    1

--- lodash/linshiqiao-lodash.js
+++ lodash/linshiqiao-lodash.js
@@ -6,24 +6,12 @@ var linshiqiao = {
       return array
     }
     var number = array.length / size
-    function ceil(number) {
-      var counter = 0
-      while (number > 1) {
-        number--
-        counter++
-      }
-      if (number > 0) {
-        counter++
-      }
-      return counter
-    }
-    number = ceil(number)
+    number = Math.ceil(number)
     var point = 1
     var cut = 0
     for (var i = 0; i < number; i++) {
       var ary = []
       while (point - cut <= size && point <= array.length) {
-
         ary.push(array[(point++) - 1])
       }
       arrays.push(ary)
